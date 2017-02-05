5d629a8daca005fd5d5afb3b10ef34546146e534
CF12 <brianx0011@gmail.com>
2017-02-05 22:46:12 +0000

    DONE - Updated API

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
 const express = require("express")
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+const port = 8080
+app.get('/', (req, res) => {
+  res.send('Hi!')
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+app.get('/*', (req, res) => {
+  var input = req.url.slice(1).split('%20')
+  var date
+
+  date = new Date(parseInt(input))
+  if (isNaN(date.getTime())) date = new Date(input)
+
+  if(isNaN(date.getTime())) {
+    res.status(400)
+    res.send('ERROR: 400 - Bad Request')
+    return
+  }
+
+  var sendback = {
+    original_url: String(req.url),
+    calendar_time: date.toUTCString(),
+    unix_time: date.getTime()
+  }
+  
+  res.status(200)
+  res.send(sendback)
+})
+
+app.listen(port, () => {
+  console.log('INFO >> Express server running on port: ' + port)
 })
